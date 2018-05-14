308c6836d530081f969111d841001de45084f196
kolibreath <734780178@qq.com>
2018-05-14 19:41:51 +0800

    initial commit

---
+++ app/src/main/java/com/example/kolibreath/muserunnerdemo/MusicPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.example.kolibreath.muserunnerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
